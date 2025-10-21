10f5b74373f39b63a6c14d23ce2a578ebc506fcb
23120096 <viettoan110905@gmail.com>
2025-10-21 10:37:49 +0700

    Yeu cau 7: dinh dang Drag+drog

---
+++ app.js
@@ -0,0 +1,52 @@
+function setNavActive(i){
+    $('#main-nav .nav-item, #site-footer .nav-item').removeClass('active');
+    $('#main-nav .nav-item[data-i="'+i+'"], #site-footer .nav-item[data-i="'+i+'"]').addClass('active');
+  }
+  $('#main-nav .nav-item, #site-footer .nav-item').on('mouseenter click', function(){
+    var i = $(this).data('i');
+    setNavActive(i);
+  });
+
+  var $newsList = $('#news-list');
+  $newsList.on('click', '.news-toggle', function(e){
+    var item = $(this).closest('.news-item');
+    item.toggleClass('collapsed');
+    $(this).text(item.hasClass('collapsed') ? '▶' : '↓');
+  });
+  (function(){
+    var dragging = null;
+    var placeholder = $('<div class="news-placeholder"></div>');
+    $newsList.on('mousedown', '.news-drag', function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      var $item = $(this).closest('.news-item');
+      dragging = { $item: $item };
+      dragging.$clone = $item.clone().addClass('dragging-clone').css({
+        width: $item.outerWidth(),
+        left: $item.offset().left,
+        top: e.pageY - 8
+      }).appendTo('body');
+      placeholder.insertAfter($item);
+      $item.hide();
+      $(document).on('mousemove.news', function(ev){
+        dragging.$clone.css({ top: ev.pageY - 8 });
+        var placed = false;
+        $newsList.children('.news-item').not($item).each(function(){
+          var $n = $(this);
+          var mid = $n.offset().top + $n.outerHeight()/2;
+          if(ev.pageY < mid){
+            placeholder.insertBefore($n);
+            placed = true; return false;
+          }
+        });
+        if(!placed) $newsList.append(placeholder);
+      });
+      $(document).on('mouseup.news', function(ev){
+        $(document).off('.news');
+        dragging.$clone.remove();
+        placeholder.replaceWith(dragging.$item);
+        dragging.$item.show();
+        dragging = null;
+      });
+    });
+  })();
